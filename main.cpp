731aa11f67719571ba0125361fec320a15f44c29
Pavlo Koval <koval.pavlo03@gmail.com>
2022-07-28 17:55:47 +0300

    Added test creation of acc

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main()
 
     json listOfRequests;
 
+    listOfRequests.push_back(createRequest.createNewAccount(keyPair));
     listOfRequests.push_back(createRequest.createNewTopic(keyPair, "topicName", "topicDescription", "topicID"));
     listOfRequests.push_back(createRequest.createNewTopic(keyPair, "topicName", "topicDescription"));
     listOfRequests.push_back(createRequest.createNewPublicMessage(keyPair, "Hello", "Hello world"));
